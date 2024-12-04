b228e7eb8408ea46f83fe968d9b8c7afac5a8424
ZaraZoha786 <mzarabha@gmail.com>
2024-12-04 20:01:31 +0200

    multiples exceptions

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ try:
 
 except ValueError as ex:
     print("Exception:", ex)
+
+try:
+    num1, num2 = eval(input("Enter two numbers, seperated by a comma :"))
+    result = num1 / num2
+    print("Result is", result)
+
+except ZeroDivisionError:
+    print("Division by zero is error !!")
+
+except SyntaxError:
+    print("Comma is missing.Enter numbers seperated by comma like this 1, 2")
+
+except:
+    print("Wrong input")
+
+else:
+    print("No exceptions")
+
+finally:
+    print("This will execute no matter what")
