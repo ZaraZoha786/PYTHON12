eaeba059d7f8f892f839ecfca87657c48138c20e
ZaraZoha786 <mzarabha@gmail.com>
2024-12-04 20:07:42 +0200

    byebye

--- main.py
+++ main.py
@@ -24,3 +24,15 @@ else:
 
 finally:
     print("This will execute no matter what")
+
+valid = False
+while not valid:
+  
+  try:
+      n=int(input("Enter a number: "))
+      while n%2==0:
+
+        print("bye")
+      valid = True
+  except ValueError:
+    print("Invalid")
